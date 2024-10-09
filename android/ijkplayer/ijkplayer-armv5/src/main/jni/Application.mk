d7f30f4a0ee64d540414de359adfc8a361c1bffd
HappyCoder <>
2024-10-09 17:39:57 +0800

    feat:ndk upgrade to 21.x & gradle upgrade to 7.5
    add ijkplayer-cmake module
    
    update:ffmpeg to 6.1
    
    add module ijkplayer-cmake
    
    update:ffmpeg to ff4.0

--- android/ijkplayer/ijkplayer-armv5/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-armv5/src/main/jni/Application.mk
@@ -28,5 +28,5 @@ APP_STL := stlport_static
 APP_CFLAGS := -O3 -Wall -pipe \
     -ffast-math \
     -fstrict-aliasing -Werror=strict-aliasing \
-    -Wno-psabi -Wa,--noexecstack \
+
     -DANDROID -DNDEBUG
--- android/ijkplayer/ijkplayer-armv7a/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-armv7a/src/main/jni/Application.mk
@@ -18,15 +18,14 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 APP_OPTIM := release
-APP_PLATFORM := android-9
+APP_PLATFORM := android-17
 APP_ABI := armeabi-v7a
 NDK_TOOLCHAIN_VERSION=4.9
 APP_PIE := false
 
-APP_STL := stlport_static
+APP_STL := c++_static
 
 APP_CFLAGS := -O3 -Wall -pipe \
     -ffast-math \
     -fstrict-aliasing -Werror=strict-aliasing \
-    -Wno-psabi -Wa,--noexecstack \
     -DANDROID -DNDEBUG
