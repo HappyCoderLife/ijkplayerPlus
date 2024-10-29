e3a3f67ee86413953cca6b3cf9aeb711084aee81
coder <>
2024-10-29 20:35:45 +0800

    update:ffmpeg to ff4.0

--- init-android.sh
+++ init-android.sh
@@ -19,7 +19,7 @@
 # IJK_FFMPEG_UPSTREAM=git://git.videolan.org/ffmpeg.git
 IJK_FFMPEG_UPSTREAM=https://github.com/HappyCoderLife/FFmpeg.git
 IJK_FFMPEG_FORK=https://github.com/HappyCoderLife/FFmpeg.git
-IJK_FFMPEG_COMMIT=n6.1
+IJK_FFMPEG_COMMIT=ff4.0--ijk0.8.8--20210426--001
 IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
 
 set -e
